ca38ee6a5dc47939fcda21d37274c01fd2a8de55
pburdeau <105862529+pburdeau@users.noreply.github.com>
2025-05-13 17:26:21 -0700

    Update main.py

--- AnalysisCode_python/main.py
+++ AnalysisCode_python/main.py
@@ -6,103 +6,81 @@ warnings.filterwarnings('ignore')
 from data_proc_master_func import *
 from plotting_func import *
 from generate_inputs import *
+import argparse
 
 ##############################################################################################################
 # Version: Philippine Burdeau - Updated on January 29th, 2025}
 # Author: Jeff Rutherford
 # Description: This script runs the BU methane model.
-# Notes: Modify the parameters below under "USER INPUTS" to match your desired inputs.
-##############################################################################################################
-
-# USER INPUTS: Specify key parameters here
-##############################################################################################################
-
-# Specify the target year for analysis
-year = 2020  # <-- Change this to the desired year.
-
-# Specify the file name of the input data
-input_filename = 'New_Paper.csv'  # <-- Change this to your input file (must contain province code and basin name).
-
-# Specify the number of Monte Carlo iterations for autorun
-n_trial = 10  # <-- Change this to set the number of MC trials.
-
 ##############################################################################################################
 
 
-# Binary options for results - do not change
-welloption = 1
-equipoption = 0
+def run_model(year, input_filename, n_trial, productionsource):
+    # Binary options
+    welloption = 1
+    equipoption = 0
+    Replicate = 0
+    AF = {}
+    AF_overwrite = 0
 
-# Folder names
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # This goes up one directory from the current script location
+    # Folder paths
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-inputsfolder = os.path.join(base_dir, 'Inputs')
-activityfolder = os.path.join(base_dir, 'ActivityData')
-basinmapfolder = os.path.join(base_dir, 'BasinMaps')
-drillinginfofolder = os.path.join(base_dir, 'DrillingInfo')
-drillinginfofolder2 = os.path.join(drillinginfofolder, 'Monthly_Production', 'Maps_Distributions_Paper')
-distributionsfolder = os.path.join(base_dir, 'EquipmentDistributions', 'Set21_Inputs')
-GHGRPfolder = os.path.join(base_dir, 'GHGRP_Dat')
+    inputsfolder = os.path.join(base_dir, 'Inputs')
+    activityfolder = os.path.join(base_dir, 'ActivityData')
+    basinmapfolder = os.path.join(base_dir, 'BasinMaps')
+    productionfolder = os.path.join(base_dir, 'ProductionData')
+    distributionsfolder = os.path.join(base_dir, 'EquipmentDistributions', 'Set21_Inputs')
+    GHGRPfolder = os.path.join(base_dir, 'GHGRP_Dat')
 
-# Import data inputs file
-input_filepath = os.path.join(inputsfolder, input_filename)
-raw_dat = pd.read_csv(input_filepath, header=None)
-Basin_N = raw_dat.iloc[:, 0].values  # First column for Basin numbers
-Basin_Index = raw_dat.iloc[:, 1].values  # Second column for Basin names
+    # Load input file
+    input_filepath = os.path.join(inputsfolder, input_filename)
+    raw_dat = pd.read_csv(input_filepath, header=None)
+    Basin_N = raw_dat.iloc[:, 0].values
+    Basin_Index = raw_dat.iloc[:, 1].values
 
-# Generate inputs
-# Specify file name of DrillingInfo data
-DI_filename = f'annualDF_{year}_SpatialJoin_2258.csv'
-print('Generating GHGRP data...')
-generate_ghgrp_dat(year, Basin_N, inputsfolder, GHGRPfolder, activityfolder)
-print('Generated GHGRP data, generating DrillingInfo data...')
-generate_drillinginfo_dat(year, inputsfolder, drillinginfofolder)
-print('Generated DrillingInfo data, generating wells to facility data...')
-return_wells_to_facility(year, inputsfolder, drillinginfofolder, GHGRPfolder)
-print('Generated wells to facility data.')
-# Do you want to replicate results for Rutherford et al 2021?
-Replicate = 0 # Leave = 0
+    DI_filename = f'annualDF_{year}_SpatialJoin_2258.csv'
 
-# Initialize blank AF matrix
-AF = {}
-AF_overwrite = 0
+    print('Generating GHGRP data...')
+    generate_ghgrp_dat(year, Basin_N, inputsfolder, GHGRPfolder, activityfolder)
 
-if Replicate == 1:
-    Basin_index = 1
+    print('Generating ProductionData data...')
+    generate_production_data(year, inputsfolder, productionfolder, productionsource)
 
-for i in range(len(Basin_N)):
-    print(len(Basin_N))
+    print('Generated ProductionData data, generating wells to facility data...')
+    return_wells_to_facility(year, inputsfolder, productionfolder, GHGRPfolder)
 
-    if Replicate != 1:
+    for i in range(len(Basin_N)):
         print(f"Basin = {Basin_Index[i]}...")
-    else:
-        print("Replicating Rutherford et al 2021")
 
-    print("Loading GHGRP data...")
+        print("Loading GHGRP data...")
+        GHGRP_exp, wellcounts = GHGRP_read_v3(i, Basin_Index, Basin_N, GHGRPfolder, year)
 
-    GHGRP_exp, wellcounts = GHGRP_read_v3(i, Basin_Index, Basin_N, GHGRPfolder, year)
-    print("Done loading GHGRP data...")
+        print("Loading model inputs...")
+        Activity_tranches, OPGEE_bin, Enverus_tab, AF_basin = tranche_gen_func(
+            i, Basin_Index, Basin_N, activityfolder, basinmapfolder, productionfolder, DI_filename, GHGRP_exp, Replicate
+        )
+        Activity_tranches = Activity_tranches.T
 
-    print("Loading model inputs...")
+        print("Starting model...")
+        autorun_func(n_trial, Activity_tranches, i, Basin_Index, activityfolder, distributionsfolder, AF_overwrite, AF)
 
-    Activity_tranches, OPGEE_bin, Enverus_tab, AF_basin = tranche_gen_func(
-        i, Basin_Index, Basin_N, activityfolder, basinmapfolder, drillinginfofolder, DI_filename, GHGRP_exp, Replicate
-    )
+        print("Processing results...")
+        data_proc_master_func(n_trial, welloption, equipoption, i, Basin_Index, activityfolder,
+                              productionfolder, Enverus_tab, AF_basin)
 
-    Activity_tranches = Activity_tranches.T
-    print("Model inputs generated...")
+    print("Initializing plotting functions...")
+    for i in range(len(Basin_N)):
+        plotting_func(Basin_Index, Basin_N, i, n_trial, basinmapfolder, activityfolder,
+                      productionfolder, DI_filename, year)
 
-    # Main function calls
-    print("Starting model...")
-    autorun_func(n_trial, Activity_tranches, i, Basin_Index, activityfolder, distributionsfolder, AF_overwrite, AF)
-    print("Results generated. Processing results...")
-    data_proc_master_func(n_trial, welloption, equipoption, i, Basin_Index, activityfolder, drillinginfofolder,
-                          Enverus_tab, AF_basin)
 
-print("Program finished")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run BU methane model.")
+    parser.add_argument('--year', type=int, required=True, help='Target year for analysis')
+    parser.add_argument('--input_filename', type=str, required=True, help='CSV file with basin info')
+    parser.add_argument('--n_trial', type=int, required=True, help='Number of Monte Carlo trials')
+    parser.add_argument('--productionsource', type=str, choices=['CalGEM', 'DrillingInfo'], required=True, help='Source of production data')
 
-# Plotting
-print("Initializing plotting functions...")
-
-for i in range(len(Basin_N)):
-    plotting_func(Basin_Index, Basin_N, i, n_trial, basinmapfolder, activityfolder, drillinginfofolder, DI_filename, year)
+    args = parser.parse_args()
+    run_model(args.year, args.input_filename, args.n_trial, args.productionsource)
